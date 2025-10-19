d36385aa642483c197a9ee4921bfcc5b8e8d8793
Quarter To Eleven <quartertoeleven2024@outlook.com>
2025-10-19 17:06:40 +0700

    separate flask apps

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-flask db upgrade
-gunicorn -b 0.0.0.0:5000 -w 4 "cms_api:create_app()"
+flask --app cms_api.db_migrate db upgrade
+gunicorn -b 0.0.0.0:5000 -w 4 "cms_api.catechism_app_api:create_app()"
